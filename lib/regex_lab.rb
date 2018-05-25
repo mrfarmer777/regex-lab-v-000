a636671cab75dd6b9ec351ee6c99e206e24b6bd1
Matt Farmer <mrfarmer777@gmail.com>
2018-05-25 17:40:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,6 +10,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   #regex: Find any word boundary followed by 'un' then any number of word characters then 'ing' then word boundary
   result = text.scan(/\bun\w+ing\b/)
+  return result
 end
 
 def words_five_letters_long(text)
