75bacf3e1f517651bb7cc028a984ab7ccc60e5d5
Matt Farmer <mrfarmer777@gmail.com>
2018-05-25 17:40:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  #regex: Find any word boundary followed by 'un' then any number of word characters then 'ing' then word boundary
+  result = text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
